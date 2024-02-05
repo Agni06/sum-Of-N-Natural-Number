05b1a11179ecfba2d64f89ea2c2eee4b2b9fc0d8
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-05 11:29:17 +0530

    Create single loop.java

---
+++ single loop.java
@@ -0,0 +1,16 @@
+class app
+  {
+    public static void main(String args[])
+    {
+int sum  =0;
+      int s = 10;
+      for(int i=1;i<=s;i++)
+        {
+sum+=i;
+          
+          
+        }
+      return sum;
+      
+    }
+  }
