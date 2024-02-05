0f89602bfcbf9d77970904ce9f2f83bf928c8e89
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-05 11:26:37 +0530

    Create 2 loops.java

---
+++ 2 loops.java
@@ -0,0 +1,20 @@
+class app
+  {
+    public static void main(String args[])
+    {
+int sum =0;
+      int s = 10;
+      for(int i=1;i<=s;i++)
+        {
+
+          for(int j = 1;j<=i;j++)
+            {
+sum+ = j;
+              
+            }
+
+        }
+      return sum;
+      
+    }
+  }
